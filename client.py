25c187ac2b72bd62af8efc72c0f64b6d6251d2d0
Anthony Si <anthony.si@monetatech.com>
2016-12-27 23:20:36 +0800

    fix client.py

--- client.py
+++ client.py
@@ -2,4 +2,4 @@ import zerorpc
 
 client = zerorpc.Client()
 client.connect("tcp://localhost:4242")
-print(client.price(96.2, 57.20, 0.06, 15.67, 6, 0))
+print(client.price(96.2, 57.20, 0.0006, 0.1567, 0, (2016,11,29), (2016,11,24)))
